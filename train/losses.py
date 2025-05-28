a54864c038d3999608cf55762ad9f5506b8624e6
mdmarti <miles.martinez@duke.edu>
2025-05-28 16:52:06 -0400

    fixing my broken ELBO (ha ha ha)

--- train/losses.py
+++ train/losses.py
@@ -29,14 +29,14 @@ def ELBO(target,model_output,recon_precision=1e-5):
     ### for the kl term, this is maybe not the most stable -- let's return the parameters instead of 
     ### the distribution
     entropy = dist.entropy()
-    cross_entropy = d*np.log(2*np.pi)/2 + torch.einsum('bk,bk ->b',z,z)
+    cross_entropy = d*np.log(2*np.pi)/2 + torch.einsum('bk,bk->b',z,z)/2
     
     return neg_lp.mean(), (-entropy + cross_entropy).mean()
 
 def ELBO_more_stable(target,model_output,recon_precision=1e-3):
 
-    B,d = xhat.shape
     (xhat,z,dist) = model_output
+    B,d = xhat.shape
     (mu,L,D) = dist
     L = L.squeeze() # goes from B x d x 1 -> B x d
 
@@ -44,6 +44,11 @@ def ELBO_more_stable(target,model_output,recon_precision=1e-3):
     neg_lp = torch.einsum('bd,bd->b',err,err) *(recon_precision)/2 + \
         d*np.log(2*np.pi)/2 - d * np.log(recon_precision)/2 
 
-    KL = 1/2 *(-torch.log(d)*(1 + torch.einsum('bd,bd->b',L/D,L)) -1 + (L**2 + D)+mu**2).sum(dim=1)
+    t12 = -1/2 *torch.log(D).sum(dim=-1) - 1/2*torch.log((1 + torch.einsum('bd,bd->b',L/D,L)))#torch.log(torch.prod(D,dim=-1)*(1 + torch.einsum('bd,bd->b',L/D,L)))
+    t22 = 1/2 * (D.sum(dim=-1) + (L**2).sum(dim=-1))
+    t32 = - d/2
+    t42 = 1/2 * (mu**2).sum(dim=-1)
 
-    return neg_lp.mean(),KL.mean()
+    kl = (t12 + t22 + t32 + t42)
+
+    return neg_lp.mean(),kl.mean()
