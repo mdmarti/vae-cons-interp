d80bcd704074dfdf400c52693d5bc042a2f41007
mdmarti <miles.martinez@duke.edu>
2025-05-30 17:42:54 -0400

    updating all my metrics

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,14 +1,21 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression as LR
 from sklearn.metrics import pairwise_distances 
-from scipy.linalg import sqrtm
+from scipy.linalg import sqrtm,cholesky,eigh
 from sklearn.neighbors import NearestNeighbors as NN
 from scipy.stats import mode 
 
 def fidelity(Kx,Ky):
 
-    sqrtx = sqrtm(Kx)
-    sqrtxy = sqrtm(sqrtx @ Ky @ sqrtx)
+    Dx, Vx = eigh(Kx)
+
+    Dx = np.clip(Dx,a_min=0,a_max=None)
+    
+    sqrtx = (Vx * np.sqrt(Dx)) @ Vx.T
+
+    Dxy,Vxy = eigh(sqrtx @ Ky @ sqrtx)
+    Dxy = np.clip(Dxy,a_min=0,a_max=None)
+    sqrtxy = (Vxy * np.sqrt(Dxy)) @ Vxy.T
 
     return np.trace(sqrtxy)
 
@@ -20,15 +27,18 @@ def neural_bures_sim(X,Y):
     c1 = np.eye(n1) - np.ones((n1,n1))/n1 
     c2 = np.eye(n2) - np.ones((n2,n2))/n2
 
+    
     Kx,Ky = c1 @ X @ X.T @ c1, c2 @ Y @ Y.T @ c2 
 
     fid = fidelity(Kx,Ky)
+    #print(fid)
 
     return fid/np.sqrt(np.trace(Kx)*np.trace(Ky))
 
 def global_linear_error(X,Y):
 
-      yhat = LR().fit_predict(X,Y)
+      model = LR().fit(X,Y)
+      yhat = model.predict(X)
 
       err = np.linalg.norm(yhat - Y,axis=1)
 
@@ -39,17 +49,19 @@ def find_nearest_neighbors(X,Y,n_neighbors=20):
     dists_x = pairwise_distances(X,n_jobs=2)
     dists_y = pairwise_distances(Y,n_jobs=2)
 
-    nbrs_x = NN(n_neighbors=n_neighbors + 1,algorithm='ball_tree',metric='precomputed').fit(dists_x)
-    nbrs_y = NN(n_neighbors=n_neighbors + 1,algorithm='ball_tree',metric='precomputed').fit(dists_y)
+    nbrs_x = NN(n_neighbors=n_neighbors + 1,algorithm='auto',metric='precomputed').fit(dists_x)
+    nbrs_y = NN(n_neighbors=n_neighbors + 1,algorithm='auto',metric='precomputed').fit(dists_y)
 
-    top_neighbors_x,_ = nbrs_x.neighbors(dists_x)
-    top_neighobrs_y,_ = nbrs_y.neighbors(dists_y)
-    top_neighbors_x,top_neighobrs_y = top_neighbors_x[:,1:],top_neighobrs_y[:,1:]
+    _,top_neighbors_x = nbrs_x.kneighbors(dists_x)
+    _,top_neighbors_y = nbrs_y.kneighbors(dists_y)
+    
+    top_neighbors_x,top_neighbors_y = top_neighbors_x[:,1:],top_neighbors_y[:,1:]
 
     total_overlap = np.zeros((X.shape[0],))
+    
     for jj in range(n_neighbors):
-         
-         total_overlap += (top_neighbors_x[:,jj:jj+1] == top_neighobrs_y).sum(axis=1)
+
+         total_overlap += (top_neighbors_x[:,jj:jj+1] == top_neighbors_y).sum(axis=1)
 
     perc_overlap = total_overlap/n_neighbors
 
@@ -83,6 +95,18 @@ def assess_gmm_fit(y,yhat):
         recalls.append(rec)
 
     return precisions,recalls
+
+def get_all_stats(true_latents,pred_latents,true_labels,pred_labels):
+
+
+    nbs = neural_bures_sim(true_latents,pred_latents)
+    global_lin_errs = global_linear_error(true_latents,pred_latents)
+    nn_neighors = find_nearest_neighbors(true_latents,pred_latents)
+
+    precision,recall = assess_gmm_fit(true_labels,pred_labels)
+
+    return nbs,global_lin_errs,nn_neighors,precision,recall
+
 
 def get_all_stats(true_latents,pred_latents,true_labels,pred_labels):
 
